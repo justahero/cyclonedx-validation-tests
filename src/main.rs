4b148860c779c2c992e0171c921cde9d1299efac
Sebastian Ziebell <sebastian.ziebell@ferrous-systems.com>
2024-02-05 12:01:48 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+#[derive(Debug, Clone)]
+pub enum ValidationEntry {
+}
+
+#[derive(Debug, Clone)]
+pub struct ValidationTree {
+    pub value: ValidationEntry,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
