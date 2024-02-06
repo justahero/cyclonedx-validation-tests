88bf623d3c720a28c5e9d0678bd25f658c1b4920
Sebastian Ziebell <sebastian.ziebell@ferrous-systems.com>
2024-02-06 15:07:10 +0100

    Expand example to test better validation logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,37 @@
 mod validation;
 
+use validation::{SpecVersion, Validate, ValidationErrors, ValidationError};
+
+fn validate_timestamp(input: &str) -> Result<(), validation::ValidationError> {
+    if input.contains("a") {
+        return Err(ValidationError::new("timestamp contains char 'a'"));
+    }
+
+    Ok(())
+}
+
+fn validate_string(input: &str) -> Result<(), validation::ValidationError> {
+    if input.len() > 4 {
+        return Err(ValidationError::new("String is too large"));
+    }
+
+    Ok(())
+}
+
 #[derive(Debug)]
 pub struct Tool {
     pub vendor: Option<String>,
     pub name: Option<String>,
+}
+
+impl Validate for Tool {
+    fn validate(
+        &self,
+        version: validation::SpecVersion,
+        parent: Result<(), ValidationErrors>,
+    ) -> Result<(), ValidationErrors> {
+        todo!()
+    }
 }
 
 #[derive(Debug)]
@@ -12,15 +40,83 @@ pub struct Metadata {
     pub tools: Option<Vec<Tool>>,
 }
 
+impl Validate for Metadata {
+    fn validate(
+        &self,
+        version: SpecVersion,
+        parent: Result<(), ValidationErrors>,
+    ) -> Result<(), ValidationErrors> {
+        todo!()
+    }
+}
+
 #[derive(Debug)]
 pub struct Bom {
     pub serial_number: Option<String>,
     pub meta_data: Option<Metadata>,
 }
 
+/// The implementation should be easy to digest
+impl Validate for Bom {
+    fn validate(
+        &self,
+        version: validation::SpecVersion,
+        parent: Result<(), ValidationErrors>,
+    ) -> Result<(), ValidationErrors> {
+        // TODO: make this nice
+        /*
+        match version {
+            SpecVersion::V1_5 => {
+                ValidationContextBuilder::new()
+                    .add_field("serial_number", validate_string(self.serial_number))
+                    .add_struct("metadata", self.meta_data.validate())
+                    .into()
+            },
+            _ => { todo!() }
+        }
+        */
+        unimplemented!()
+    }
+}
+
+/// Validates the bom according to a given [`SpecVersion`].
+pub fn validate_bom(version: SpecVersion, bom: Bom) -> Result<(), ValidationErrors> {
+    bom.validate(version, std::result::Result::Ok(()))
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::{validate_bom, validation::SpecVersion, Bom, Metadata, Tool};
+
     #[test]
-    fn it_validates() {
+    fn validate_succeeds() {
+        let bom = Bom {
+            serial_number: Some("1234".to_string()),
+            meta_data: Some(Metadata {
+                timestamp: Some(String::from("2024-01-02")),
+                tools: Some(vec![Tool {
+                    vendor: Some(String::from("Vendor")),
+                    name: Some(String::from("dig")),
+                }]),
+            }),
+        };
+
+        assert!(validate_bom(SpecVersion::V1_3, bom).is_ok());
+    }
+
+    #[test]
+    fn validate_fails() {
+        let bom = Bom {
+            serial_number: Some("1234".to_string()),
+            meta_data: Some(Metadata {
+                timestamp: Some(String::from("2024-01-02")),
+                tools: Some(vec![Tool {
+                    vendor: Some(String::from("Vendor")),
+                    name: Some(String::from("dig")),
+                }]),
+            }),
+        };
+
+        assert!(validate_bom(SpecVersion::V1_4, bom).is_err());
     }
 }
--- src/validation.rs
+++ src/validation.rs
@@ -10,7 +10,11 @@ pub enum SpecVersion {
 }
 
 pub trait Validate {
-    fn validate(&self, version: SpecVersion) -> Result<(), ValidationErrors>;
+    fn validate(
+        &self,
+        version: SpecVersion,
+        parent: Result<(), ValidationErrors>,
+    ) -> Result<(), ValidationErrors>;
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -28,13 +32,19 @@ impl ValidationError {
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum ValidationErrorsKind {
+    /// Collects all field validation errors in context of a struct
     Struct(Box<ValidationErrors>),
+    /// Collects all child elements in context of a list, e.g. `Vec`
     List(BTreeMap<usize, Box<ValidationErrors>>),
+    /// Contains the list of validation errors for a single field, e.g. struct field.
     Field(Vec<ValidationError>),
+    /// Represents an error for an Enum variant.
+    Enum(ValidationError),
 }
 
 #[derive(Debug, Clone, PartialEq, Default)]
 pub struct ValidationErrors {
+    /// Maps a name to a set of context errors.
     inner: IndexMap<String, ValidationErrorsKind>,
 }
 
@@ -46,13 +56,15 @@ impl ValidationErrors {
         }
     }
 
-    pub fn has_error(result: &Result<(), ValidationErrors>, field: &str) -> bool {
-        match result {
-            Ok(()) => false,
-            Err(ref errors) => errors.contains_key(field),
-        }
+    /// Returns new [`ValidationContext`] with results for all nested fields.
+    pub fn merge(
+        field: &str,
+        child: Result<(), ValidationErrors>,
+    ) -> Result<(), ValidationErrors> {
+        todo!("")
     }
 
+    /// Adds a single [`ValidationError`] to a named struct field.
     pub fn add_field(&mut self, field: &str, error: ValidationError) {
         if let ValidationErrorsKind::Field(ref mut vec) = self
             .inner
@@ -61,18 +73,36 @@ impl ValidationErrors {
         {
             vec.push(error);
         } else {
-            panic!("Found a non-field ValidationErrorsKind instance");
+            panic!("Found a non-field ValidationErrorsKind");
+        }
+    }
+
+    /// Adds a single [`ValidationError`] associated with an enum variant.
+    pub fn add_enum(&mut self, variant: &str, error: ValidationError) {
+        self.inner
+            .entry(variant.to_string())
+            .or_insert_with(|| ValidationErrorsKind::Enum(error));
+    }
+
+    pub fn has_error(result: &Result<(), ValidationErrors>, field: &str) -> bool {
+        match result {
+            Ok(()) => false,
+            Err(ref errors) => errors.contains_key(field),
         }
     }
 
     pub fn contains_key(&self, key: &str) -> bool {
         self.inner.contains_key(key)
     }
+
+    pub fn is_empty(&self) -> bool {
+        self.inner.is_empty()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{ValidationError, ValidationErrors};
+    use super::{ValidationErrors, ValidationError};
 
     #[test]
     fn has_error() {
@@ -91,5 +121,14 @@ mod tests {
 
         assert!(errors.contains_key("test"));
         assert!(!errors.contains_key("haha"));
+    }
+
+    #[test]
+    fn is_empty() {
+        let mut errors = ValidationErrors::new();
+        assert!(errors.is_empty());
+
+        errors.add_field("hello", ValidationError::new("again"));
+        assert!(!errors.is_empty());
     }
 }
